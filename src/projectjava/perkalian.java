54c754d067bf9deb67c651a3d5dd60fbe4ddd0ea
SBC4 <SBC4@SBC03>
2015-04-28 14:18:29 +0700

    perkalian

---
+++ src/projectjava/perkalian.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectjava;
+
+
+public class perkalian {
+    
+   public static void main(String[] args) {
+        int a,b,total;
+        a=10;
+        b=15;
+        
+        total=a*b;
+        System.out.println("hasilnya adalah " +total);
+    }
+        
+     
+}
