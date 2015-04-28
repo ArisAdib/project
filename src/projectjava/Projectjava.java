1a99fd4360540caaec99d0c9846133faf40fcadb
SBC4 <SBC4@SBC07>
2015-04-28 13:43:38 +0700

    belajarjava

---
+++ src/projectjava/Projectjava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectjava;
+
+/**
+ *
+ * @author SBC4
+ */
+public class Projectjava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a,b,c,total;
+        a=10;
+        b=15;
+        c=25;
+        total=a+b+c;
+        System.out.println("hasilnya adalah" +total);
+    }
+}
